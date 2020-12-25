a67b3fd0c57c0bcd2dbd13c83b0e1c78115558a2
Mohammed Muzzammil <36696171+mohammed-muzzammil@users.noreply.github.com>
2020-12-25 19:02:35 +0530

    Update st1.py

--- st1.py
+++ st1.py
@@ -643,7 +643,7 @@ def mail():
                 file.close()
 
         sender_address = 'dpreprocessing@gmail.com'
-        st.write(sender_pass)
+  
         regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
         receiver_address = st.text_input("Please Enter The Email Address")
         if receiver_address:
